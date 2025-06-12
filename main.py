53380d2816afafab55979166c7b17f229b2cfec4
Buoyancy <oluwaseyidavid2005@gmail.com>
2025-06-12 12:11:06 +0100

    Update main.py for json

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, UploadFile, Form
 from fastapi.responses import JSONResponse
 import os
+import json
 from generate_video import generate_azure_tts, create_did_talk, check_talk_status, preprocess_image
 
 app = FastAPI()
